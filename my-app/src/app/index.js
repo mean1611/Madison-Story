d30f09136c9b6fd6f007b44a2383776063808704
mean1611 <maen1611@gmail.com>
2025-02-21 23:21:17 +1100

    Update index.js

--- my-app/src/app/index.js
+++ my-app/src/app/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./router";
 
+document.title = "Madison Story";
+
 ReactDOM.render(
   <React.StrictMode>
     <AppRouter />
